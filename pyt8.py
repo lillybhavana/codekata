c3c3682059862db23877492ee538a59c746456ac
lillybhavana <55087433+lillybhavana@users.noreply.github.com>
2019-09-09 16:14:25 +0530

    Add files via upload

---
+++ pyt8.py
@@ -0,0 +1,14 @@
+a=[1,2,3,4,5,6,7,8]
+b=0
+c=0
+for i in range(8):
+    if(a[i]%2==0):
+        b=b+a[i]
+    if(a[i]%2!=0):
+        c=c+a[i]
+print(b)
+print(c)
+d=b-c
+print(d)
+        
+        
